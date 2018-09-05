0e5fa04ed9a63b7209a1dfc0307eef4311461611
Jorgefr <jorgefr_inguitar@hotmail.com>
2018-09-04 20:57:36 -0500

    4Sep2018

--- test1/config/routes.rb
+++ test1/config/routes.rb
@@ -6,4 +6,13 @@ Rails.application.routes.draw do
   resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  
+  namespace 'api' do
+    namespace 'v1' do
+      post 'auth_user' => 'authentication#authenticate_user'
+    end
+  end
+
 end
+
+
